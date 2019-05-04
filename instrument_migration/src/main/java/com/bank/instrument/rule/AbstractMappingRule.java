5e94e791b5f1aca6567b6902c69689517cddce1b
unknown <1032774144@qq.com>
2019-05-04 12:21:50 +0800

    fix match rule

--- instrument_migration/src/main/java/com/bank/instrument/rule/AbstractMappingRule.java
+++ instrument_migration/src/main/java/com/bank/instrument/rule/AbstractMappingRule.java
@@ -31,6 +31,7 @@ public abstract class AbstractMappingRule implements Rule {
 				internalPublishDto.setMarket(exchangePublishDto.getMarket());
 				internalPublishDto.setLabel(exchangePublishDto.getLabel());
 				internalPublishDto.setTradable(exchangePublishDto.isTradable());
+				internalPublishDto.setPublishCode(exchangePublishDto.getPublishCode());
 				internalPublishes.add(internalPublishDto);
 			}
 		} else if (basePublishDto instanceof PublishDto) {
@@ -45,6 +46,7 @@ public abstract class AbstractMappingRule implements Rule {
 				internalPublishDto.setMarket(publishDto.getMarket());
 				internalPublishDto.setLabel(publishDto.getLabel());
 				internalPublishDto.setTradable(true);
+				internalPublishDto.setPublishCode(publishDto.getPublishCode());
 				internalPublishes.add(internalPublishDto);
 			}
 		}
@@ -53,12 +55,13 @@ public abstract class AbstractMappingRule implements Rule {
 	@Override
 	public InternalPublishDto matchInternalPublishByRules(BasePublishDto basePublishDto,
 			Collection<InternalPublishDto> internalPublishes) {
-		return internalPublishes.stream().filter(internalPublishDto->ruleFilter(basePublishDto, internalPublishDto)).findFirst().orElse(null);
+		return internalPublishes.stream().filter(internalPublishDto -> ruleFilter(basePublishDto, internalPublishDto))
+				.findFirst().orElse(null);
 	}
 
-	
 	/**
-	 * filter 
+	 * filter
+	 * 
 	 * @param basePublishDto
 	 * @param internalPublishDto
 	 * @return
@@ -66,20 +69,26 @@ public abstract class AbstractMappingRule implements Rule {
 	private boolean ruleFilter(BasePublishDto basePublishDto, InternalPublishDto internalPublishDto) {
 		Map<MappingKeyEnum, MappingKeyEnum> rules = getMappingRules();
 		boolean allRulesMatched = true;
-		for(Entry<MappingKeyEnum, MappingKeyEnum> ruleEntry:rules.entrySet()) {
-			String externalKey = ruleEntry.getKey().getKey();
-			String internalKey = ruleEntry.getValue().getKey();
+		for (Entry<MappingKeyEnum, MappingKeyEnum> ruleEntry : rules.entrySet()) {
+			String internalKey = ruleEntry.getKey().getKey();
+			String externalKey = ruleEntry.getValue().getKey();
 			try {
 				Class<?> internalClazz = Class.forName(InternalPublishDto.class.getName());
-				Method internalMethod = internalClazz.getDeclaredMethod(internalKey);
+				Method internalMethod = null;
+				try {
+					internalMethod = internalClazz.getDeclaredMethod(internalKey);
+				} catch (Exception e) {
+					internalMethod = internalClazz.getSuperclass().getDeclaredMethod(internalKey);
+				}
 				Object internalMapKey = internalMethod.invoke(internalPublishDto);
-				Object externalMapKey = getExternalMappingKey(basePublishDto, externalKey, internalClazz, internalMethod);
+				Object externalMapKey = getExternalMappingKey(basePublishDto, externalKey, internalClazz,
+						internalMethod);
 				if (!(internalMapKey != null && externalMapKey != null && internalMapKey.equals(externalMapKey))) {
-					allRulesMatched=false;
+					allRulesMatched = false;
 					break;
 				}
 			} catch (Exception e) {
-				allRulesMatched=false;
+				allRulesMatched = false;
 				break;
 			}
 		}
@@ -89,15 +98,25 @@ public abstract class AbstractMappingRule implements Rule {
 	private String getExternalMappingKey(BasePublishDto basePublishDto, String externalKey, Class<?> internalClazz,
 			Method internalMethod)
 			throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-		String externalMappingkey=null;
+		String externalMappingkey = null;
 		if (basePublishDto instanceof ExchangePublishDto) {
 			Class<?> externalClazz = Class.forName(ExchangePublishDto.class.getName());
-			Method externalMethod = externalClazz.getDeclaredMethod(externalKey);
-			externalMappingkey = (String)externalMethod.invoke((ExchangePublishDto) basePublishDto);
+			Method externalMethod = null;
+			try {
+				externalMethod = externalClazz.getDeclaredMethod(externalKey);
+			} catch (Exception e) {
+				externalMethod = externalClazz.getSuperclass().getDeclaredMethod(externalKey);
+			}
+			externalMappingkey = (String) externalMethod.invoke((ExchangePublishDto) basePublishDto);
 		} else if (basePublishDto instanceof PublishDto) {
 			Class<?> externalClazz = Class.forName(ExchangePublishDto.class.getName());
-			Method externalMethod = externalClazz.getDeclaredMethod(externalKey);
-			externalMappingkey = (String)externalMethod.invoke((ExchangePublishDto) basePublishDto);
+			Method externalMethod = null;
+			try {
+				externalMethod = externalClazz.getDeclaredMethod(externalKey);
+			} catch (Exception e) {
+				externalMethod = externalClazz.getSuperclass().getDeclaredMethod(externalKey);
+			}
+			externalMappingkey = (String) externalMethod.invoke((ExchangePublishDto) basePublishDto);
 		}
 		return externalMappingkey;
 	}
